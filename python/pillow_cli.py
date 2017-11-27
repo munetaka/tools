20390cef759688d4c884487e2ceb9933b78c951e
Munetaka Mizutani <mu.mizutani@gmail.com>
2017-11-27 13:42:07 +0900

    fix var: using im as pillow obj

--- python/pillow_cli.py
+++ python/pillow_cli.py
@@ -10,22 +10,23 @@ import numpy as np
 @click.pass_context
 def cli(ctx, out):
     ctx.obj['out'] = out
-    pass
 
 
 @click.pass_context
-def save(ctx, image):
+def save(ctx, img):
     if ctx.obj.get('out') is not None:
         click.echo('output to {}'.format(ctx.obj.get('out')))
-        image.save(ctx.obj.get('out'))
+        img.save(ctx.obj.get('out'))
+    else:
+        img.show()
 
 
 def convert_array_to_image(array):
     return Image.fromarray(np.uint8(array))
 
 
-def convert_pil_to_array(image):
-    return np.asarray(image)
+def convert_pil_to_array(img):
+    return np.asarray(img)
 
 
 @cli.command(short_help='resize image')
@@ -33,9 +34,9 @@ def convert_pil_to_array(image):
 @click.argument('size', type=click.Path(exists=True), required=True)
 def resize(image, size):
     '''resize IMAGE SIZE. SIZE is tuple (width, height)'''
-    image = Image.open(image)
-    image.resize(size)
-    save(image)
+    im = Image.open(image)
+    im.resize(size)
+    save(im)
 
 
 @cli.command(short_help='blend images')
@@ -44,10 +45,10 @@ def resize(image, size):
 @click.argument('alpha', default=0.5)
 def blend(image1, image2, alpha):
     '''blend IMAGE1 and IMAGE2.'''
-    image1 = Image.open(image1)
-    image2 = Image.open(image2)
-    blended_image = Image.blend(image1, image2, alpha)
-    save(blended_image)
+    im1 = Image.open(image1)
+    im2 = Image.open(image2)
+    blended_im = Image.blend(im1, im2, alpha)
+    save(blended_im)
 
 
 @cli.command(short_help='composite images')
@@ -56,60 +57,60 @@ def blend(image1, image2, alpha):
 @click.argument('mask', default=0.5)
 def composite(image1, image2, mask):
     '''composite IMAGE1 and IMAGE2'''
-    image1 = Image.open(image1)
-    image2 = Image.open(image2)
+    im1 = Image.open(image1)
+    im2 = Image.open(image2)
     mask = Image.open(mask).convert('1')
-    composited_image = Image.composite(image1, image2, mask)
-    save(composited_image)
+    composited_im = Image.composite(im1, im2, mask)
+    save(composited_im)
 
 
 @cli.command()
 def create():
     '''create tiny sample image'''
     array = np.eye(10) * 255
-    image = convert_array_to_image(array)
-    save(image)
+    im = convert_array_to_image(array)
+    save(im)
 
 
 @cli.command()
 @click.argument('image', type=click.Path(exists=True), required=True)
 @click.option('--equalize', '-e', is_flag=True)
 def grayscale(image, equalize):
-    image = Image.open(image)
+    im = Image.open(image)
     if equalize is True:
-        image = ImageOps.equalize(image)
-    image = image.convert("L")
-    save(image)
+        im = ImageOps.equalize(image)
+    im = im.convert("L")
+    save(im)
 
 
 @cli.command()
 @click.argument('image', type=click.Path(exists=True), required=True)
 @click.option('--equalize', '-e', is_flag=True)
 def threshold(image, equalize):
-    image = Image.open(image)
+    im = Image.open(image)
     if equalize is True:
-        image = ImageOps.equalize(image)
-    image = image.convert("L")
-    image = image.point(lambda x: 0 if x < 230 else x)
-    save(image)
+        im = ImageOps.equalize(im)
+    im = im.convert("L")
+    im = im.point(lambda x: 0 if x < 230 else x)
+    save(im)
 
 
 @cli.command()
 @click.argument('image', type=click.Path(exists=True), required=True)
 def equalize(image):
     '''equalize image'''
-    image = Image.open(image)
-    image = ImageOps.equalize(image)
-    save(image)
+    im = Image.open(image)
+    im = ImageOps.equalize(im)
+    save(im)
 
 
 @cli.command()
 @click.argument('image1', type=click.Path(exists=True), required=True)
 @click.argument('image2', type=click.Path(exists=True), required=True)
 def difference(image1, image2):
-    image1 = Image.open(image1)
-    image2 = Image.open(image2)
-    click.echo(ImageChops.difference(image1, image2).getbbox())
+    im1 = Image.open(image1)
+    im2 = Image.open(image2)
+    click.echo(ImageChops.difference(im1, im2).getbbox())
 
 
 def main():
