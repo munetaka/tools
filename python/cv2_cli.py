04ec17624a0a06cad7670804ae8300d147ff1468
Munetaka Mizutani <mu.mizutani@gmail.com>
2017-12-19 18:37:00 +0900

    erosion

--- python/cv2_cli.py
+++ python/cv2_cli.py
@@ -16,7 +16,7 @@ def save(ctx, img):
         click.echo('output to {}'.format(ctx.obj.get('out')))
         cv2.imwrite(ctx.obj.get('out'), img)
     else:
-        cv2.imshow('image',img)
+        cv2.imshow('image', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -36,7 +36,7 @@ def filter():
 
 @filter.command()
 @click.argument('image', type=click.Path(exists=True), required=True)
-@click.option('--ksize', '-k', nargs=2, type=click.Tuple([int, int]), default=(3,3))
+@click.option('--ksize', '-k', nargs=2, type=click.Tuple([int, int]), default=(3, 3))
 def blur(image, ksize):
     im = cv2.imread(image)
     im = cv2.blur(im, ksize=ksize)
@@ -67,6 +67,23 @@ def sharpen(image, pillow_kernel):
         -1,
         kernel
     )
+    save(im)
+
+
+@filter.command()
+@click.argument('image', type=click.Path(exists=True), required=True)
+@click.option('--neiborhood', '-n', type=click.Choice([4, 8]), default=4)
+def erosion(image, neiborhood):
+    im = cv2.imread(image, 0)
+    if neiborhood == 4:
+        kernel = np.array([
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ], np.uint8)
+    else:
+        kernel = np.ones((5, 5), np.uint8)
+    im = cv2.erode(im, kernel, iterations=1)
     save(im)
 
 
